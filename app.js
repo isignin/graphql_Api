8ecf1e9336cf262e9e18002be6ffe6f2ced6313f
Steve Loo <isignin@gmail.com>
2019-05-15 22:51:05 -0400

    Added graphql schema

--- app.js
+++ app.js
@@ -3,13 +3,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-// Handle CORS error by allowing
+//Handle CORS error by allowing
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept,Authorization");
@@ -19,6 +21,33 @@ app.use((req, res, next) => {
     }
     next();
  });
+
+ app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Romantic Cooking', 'Sailing', 'All-night coding']
+        },
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+ }));
 
  app.use((req,res,next)=>{
     const error = new Error('Not Found!');
